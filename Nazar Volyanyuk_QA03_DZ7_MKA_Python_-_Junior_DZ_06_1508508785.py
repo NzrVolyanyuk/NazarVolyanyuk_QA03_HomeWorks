c9f3cb2e8dfcec548c3856f99691311eb6c0f4d1
Nazar <123@gmail.com>
2022-02-02 22:00:37 +0200

    3 task final

--- Nazar Volyanyuk_QA03_DZ7_MKA_Python_-_Junior_DZ_06_1508508785.py
+++ Nazar Volyanyuk_QA03_DZ7_MKA_Python_-_Junior_DZ_06_1508508785.py
@@ -3,6 +3,7 @@
 1. Улучшите игру так, чтобы после угадывания числа,выводился номер попытки,
 которую сделал пользователь для угадывания магического числа.
 
+!!!! мне кажется что это то же число что и пункте 1 !!!!!!
 2. Улучшите игру так, чтобы после угадывания числа, выводилось количество потраченных попыток,
 которые сделал пользователь для угадывания магического числа.
 
@@ -15,22 +16,32 @@
 
 #Guess my number
 import random
-# secret_number = random.randint(1,20)
-secret_number = 5
-guess_number = 0
-lifes = 3
-attempt = 0
-while secret_number != guess_number and lifes >= 0:
-    guess_number = int(input("\nEnter number from 1 to 20:"))
-    lifes = lifes - 1
-    attempt = attempt + 1
-    if lifes !=0:
-        print(f"You have {lifes} lifes")
-    if secret_number < guess_number:
-        print("Your number greater ^^^ than secret")
-    elif secret_number > guess_number:
-        print("Your number less  than secret")
-if lifes >= 0:
-    print(f"You WIN from {attempt} attempt")
-else:
-    print("you LOSE")
+game = True
+while game:
+    secret_number = 5
+    # secret_number = random.randint(1,20)
+    guess_number = 0
+    lifes = 3
+    attempt = 0
+    while secret_number != guess_number and lifes > 0:
+        guess_number = int(input("\nEnter number from 1 to 20: "))
+        lifes = lifes - 1
+        attempt = attempt + 1
+        if lifes !=0:
+            print(f"You have {lifes} lifes")
+        if secret_number < guess_number:
+            print("Your number greater ^^^ than secret")
+        elif secret_number > guess_number:
+            print("Your number less  than secret")
+    if lifes >= 0:
+        print(f"You WIN from {attempt} attempt")
+        if input("If you want continue write Y, if you want finish the game write N: ") == "Y":
+            game = True
+        else:
+            game = False
+    else:
+        print(f"you LOSE, the number that the bot guessed is {secret_number}")
+        if input("If you want continue write Y, if you want finish the game write N: ") == "Y":
+            game = True
+        else:
+            game = False
